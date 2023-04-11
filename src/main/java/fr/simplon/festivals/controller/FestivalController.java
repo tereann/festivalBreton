e25b3dad554c7cafb041a9ee4c5309fde3ef8cb6
Allison <121940573+Salas-Allison@users.noreply.github.com>
2023-04-11 15:41:46 +0200

    Ajouter

--- src/main/java/fr/simplon/festivals/controller/FestivalController.java
+++ src/main/java/fr/simplon/festivals/controller/FestivalController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FestivalController {
@@ -16,5 +18,16 @@ public class FestivalController {
     public String pageAccueil(Model model) {
         model.addAttribute("festival", new Festival());
         return "index.html";
+    }
+    @GetMapping("/ajouter")
+    public String afficherAjouter(Model model) {
+        model.addAttribute("festival", new Festival());
+        return "ajouter";
+    }
+    @PostMapping("/ajouterFestival")
+    public String enregistrerFestival(@ModelAttribute("festival") Festival festival){
+        festivalDao.saveFestival(festival.getNom(), festival.getVille(), festival.getLieu(),
+                festival.getDebut(), festival.getFin(), festival.getLat(), festival.getLon());
+        return "redirect:/";
     }
 }
