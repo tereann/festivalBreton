4833229c44b8317df547c459e5c48c1722eaf066
tereann <121940590+tereann@users.noreply.github.com>
2023-04-11 11:35:46 +0200

    Initial commit

---
+++ src/main/java/fr/simplon/festivals/FestivalBretonApplication.java
@@ -0,0 +1,13 @@
+package fr.simplon.festivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalBretonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestivalBretonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/simplon/festivals/FestivalBretonApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.simplon.festivals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalBretonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
